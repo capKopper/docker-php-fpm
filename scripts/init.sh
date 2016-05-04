2983e9bb2aa08d631a0857565b9f8be06b0e02ff
Sébastien Kurtzemann <sebastien.k@capkopper.fr>
2016-05-04 11:10:44 +0200

    Use the logging library in the init script

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,23 +1,9 @@
 #!/bin/bash
 set -eo pipefail
 
-_log(){
-  declare BLUE="\e[32m" WHITE="\e[39m" BOLD="\e[1m" NORMAL="\e[0m"
-  echo -e "$(date --iso-8601=s)${BLUE}${BOLD} (info)${WHITE}:" $@${NORMAL}
-}
 
-_error(){
-  declare RED="\e[91m" WHITE="\e[39m"
-  echo -e "$(date --iso-8601=s)${RED} (error)${WHITE}:" $@
-  exit 1
-}
-
-_debug()
-{
-  declare BLUE="\e[36m" WHITE="\e[39m"
-  echo -e "$(date --iso-8601=s)${BLUE} (debug)${WHITE}:" $@
-}
-
+# load library
+source /scripts/logging.lib.sh
 
 usage(){
   # """
